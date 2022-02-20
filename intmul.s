aa79458359f9f8516ccc736d6266e0dbc815e442
Jackson Patrick Meyers <jpmeyers@arm.csc.calpoly.edu>
2022-02-20 13:35:08 -0500

    Fix register conflicts and verify.

--- intmul.s
+++ intmul.s
@@ -7,29 +7,29 @@ intmul:
     stp x29, x30, [sp, -16]!
     mov x29, sp
 
-    mov x19, x0 // numA
-    mov x20, x1 // numB
-    mov x21, #0 // result
+    mov x23, x0 // numA
+    mov x24, x1 // numB
+    mov x25, #0 // result
 loop:
 
-    cmp x20, 0
+    cmp x24, 0
     b.eq endfunc
 
     // result += a
-    mov x0, x21
-    mov x1, x19
+    mov x0, x25
+    mov x1, x23
     bl intadd
-    mov x21, x0
+    mov x25, x0
     
     // b -= 1
-    mov x0, x20
+    mov x0, x24
     mov x1, #1
     bl intsub
-    mov x20, x0
+    mov x24, x0
 
     b loop
 
 endfunc:
-
+    mov    x0, x25
     ldp    x29, x30, [sp], 16
     ret
