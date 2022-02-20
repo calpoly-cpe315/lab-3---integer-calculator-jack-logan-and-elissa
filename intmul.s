8b0afa61580d478549654fd06b21dd7b55d33e1a
Jackson Patrick Meyers <jpmeyers@arm.csc.calpoly.edu>
2022-02-20 13:09:51 -0500

    Implement mul and sub.

--- intmul.s
+++ intmul.s
@@ -4,4 +4,32 @@
     .global intmul
 
 intmul:
+    stp x29, x30, [sp, -16]!
+    mov x29, sp
 
+    mov x19, x0 // numA
+    mov x20, x1 // numB
+    mov x21, #0 // result
+loop:
+
+    cmp x20, 0
+    b.eq endfunc
+
+    // result += a
+    mov x0, x21
+    mov x1, x19
+    bl intadd
+    mov x21, x0
+    
+    // b -= 1
+    mov x0, x20
+    mov x1, #1
+    bl intsub
+    mov x20, x0
+
+    b loop
+
+endfunc:
+
+    ldp    x29, x30, [sp], 16
+    ret
--- intsub.s
+++ intsub.s
@@ -4,4 +4,32 @@
     .global intsub
 
 intsub:
+    stp x29, x30, [sp, -16]!
+    mov x29, sp
+
+    mov x19, x0 // 1st number
+    mov x20, x1 // 2nd number
+    // x21 and x22 are used for carry
+
+loop:
+    cmp x20, #0 // continue until carry is 0
+    b.eq endloop
+    // Carry = ~x & y
+    eor x22, x19, 0xFFFFFFFF
+    and x21, x22, x20 
+
+    // XOR (or eor I guess)
+    eor x19, x20, x19
+
+    // Compute carry
+    lsl x20, x21, #1 // shift left
+    // loop
+    b loop
+
+endloop:
+   mov x0, x19
+   ldp x29, x30, [sp], 16
+   ret
+
+
 
